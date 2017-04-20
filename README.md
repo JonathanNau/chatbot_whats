95a961493bfa51c4ea1a732a92fe830c30e68453
JonathanNau <jonathan_nau@live.com>
2017-04-20 01:16:57 -0300

    Correções e novas funções(stemm, categoria). Novo BD.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 chatbot_whats
+
+Bibliotecas necessarias:
+- NLTK
+- Counter
+- CSV
+- re (expressões regulares)
+- math
+
+Funções:
+Calculo de similaridade entre frase. get_cosine
+
+Processamento das frases e interação com usuário. def processa_pergunta(sentence, APARELHO):
+
--- main.py
+++ main.py
@@ -11,8 +11,10 @@ import csv
 import re
 import math
 from collections import Counter
+from nltk.stem.snowball import SnowballStemmer
 
 WORD = re.compile(r'\w+')
+
 
 def get_cosine(vec1, vec2):
     '''
@@ -32,7 +34,7 @@ def get_cosine(vec1, vec2):
 
 def text_to_vector(text):
     '''
-    Não sei ainda
+    Cria vetor de palavras
     '''
     words = WORD.findall(text)
     return Counter(words)
@@ -40,36 +42,55 @@ def text_to_vector(text):
 
 ARCHIVE = csv.reader(open("bd.csv", "r"), delimiter=';')
 A_SPLIT = []
+STEMMER = SnowballStemmer("portuguese")
 for row in ARCHIVE:
     #print(row)
-    a = [row[0], row[1], row[2], row[3]]
+    a = [' '.join([STEMMER.stem(i) for i in row[0].split()]), row[1], row[2], row[3]]
     A_SPLIT.append(a)
 
-def processa_pergunta(sentence):
+def processa_pergunta(sentence, APARELHO):
     '''
     testar
     '''
+    sentence = ' '.join([STEMMER.stem(i) for i in sentence.split()])
     resposta = 'Não sei'
     value_cosine = 0.0
     value_feedback = 0
+    categoria = 'geral'
     vector1 = text_to_vector(sentence)
+    cat = ['geral', 'conversa']+APARELHO
     for i in enumerate(A_SPLIT):
-        if int(A_SPLIT[i[0]][3]) < 0:
+        if int(A_SPLIT[i[0]][3]) < 0 or A_SPLIT[i[0]][1].lower() not in cat:
             continue
         vector2 = text_to_vector(A_SPLIT[i[0]][0].lower())
         cosine = get_cosine(vector1, vector2)
-       # print(cosine)
-        if cosine > 0.40 and cosine > value_cosine:
+        if cosine > 0.30 and cosine > value_cosine:
             value_cosine = cosine
             value_feedback = int(A_SPLIT[i[0]][3])
             resposta = A_SPLIT[i[0]][2]
-        elif cosine == value_cosine and value_feedback < int(A_SPLIT[i[0]][3]):
+            categoria = A_SPLIT[i[0]][1].lower()
+        elif cosine > 0.0 and cosine == value_cosine and value_feedback < int(A_SPLIT[i[0]][3]):
             value_cosine = cosine
             value_feedback = int(A_SPLIT[i[0]][3])
             resposta = A_SPLIT[i[0]][2]
-    return resposta
+            categoria = A_SPLIT[i[0]][1].lower()
+    if len(APARELHO) > 1 and categoria not in ['geral', 'conversa']:
+        print('Encontramos uma resposta, mas precisamos confirmar seu aparelho')
+        print('Por acaso seu aparelho é ' + categoria + '?')
+        answer = input('User: ')
+        if answer.lower() in ['s', 'sim', 'yes']:
+            APARELHO.clear()
+            APARELHO.append(categoria)
+            return resposta
+        else:
+            APARELHO.remove(categoria)
+            print(APARELHO)
+            return processa_pergunta(sentence, APARELHO)
+    else:
+        return resposta
 
 SENTENCES = []
+APARELHO = ['android', 'iphone']
 while True:
     SENTENCE = input('User: ')
     SENTENCES.append(SENTENCE)
@@ -88,12 +109,11 @@ while True:
     elif SENTENCE.lower() in ['ajuda', 'versão']:
         print('Execução de comando')
     else:
-        PROCESS = processa_pergunta(SENTENCE.lower())
+        PROCESS = processa_pergunta(SENTENCE.lower(), APARELHO)
         SENTENCES.append(PROCESS)
         print(PROCESS)
 print('Bye!!!!')
 with open('bd.csv', 'w', newline='') as csvfile:
-    WRITER = csv.writer(csvfile, delimiter=';',
-                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    WRITER = csv.writer(csvfile, delimiter=';')
     for j in enumerate(A_SPLIT):
         WRITER.writerow(A_SPLIT[j[0]])
